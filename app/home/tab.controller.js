6c7df7a47f3c485b57a6159307fc3d353ea4eb97
fenos53 <fenos53@gmail.com>
2016-06-13 14:05:18 +0000

    Add toogle

--- app/home/tab.controller.js
+++ app/home/tab.controller.js
@@ -1,5 +1,46 @@
 export default class TabController{
-    constructor($stateParams){
-         this.user = $stateParams.user;
+    constructor($scope,$stateParams){
+         this.todos = [];
+          this.user = $stateParams.user
+         this.userText = '';
+     }
+     
+     addTodo()
+     {
+         this.todos.push({
+             value:{
+                 user: this.user,
+                 title: this.userText,
+                 completed:false
+             },
+             editing: false
+         });
+     }
+     
+     onUser()
+     {
+         this.selectedTab = 'user';
+     }
+     
+     onAll()
+     {
+         this.selectedTab = 'all';
+     }
+     
+      editTodo(todo)
+     {
+         todo.editing = !todo.editing;
+         this.originalToDo = todo;
+     }
+     
+     doneEditing(todo)
+     {
+         todo.editing = !todo.editing;
+     }
+ 
+     removeTodo(toDo)
+     {
+         var index = this.todos.indexOf(toDo);
+         this.todos.splice(index,1);
     }
 } 
