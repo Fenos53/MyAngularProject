547311bd59dba907504539dca867a31583e2be1b
fenos53 <fenos53@gmail.com>
2016-06-13 13:27:16 +0000

    Routing

--- app/index.js
+++ app/index.js
@@ -2,8 +2,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import angular from 'angular';
 import routing from './config';
 import uirouter from 'angular-ui-router';
+import home from './features/directives'; 
 
 const ngModule = angular.module('app',[
-    uirouter
+    uirouter,
+    home
     ])
     .config(routing);
