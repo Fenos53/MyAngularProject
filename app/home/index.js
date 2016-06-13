3990d9254fd1b93d7015229beffe5e2aff412dbd
fenos53 <fenos53@gmail.com>
2016-06-13 13:55:34 +0000

    Add search

--- app/home/index.js
+++ app/home/index.js
@@ -1,10 +1,15 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './home.routes';
-
+import userInput from './directives/user_input'; //directives
+import userToDo from './directives/user_toDo';
+import allToDo from './directives/all_toDo';
 import TabController from './tab.controller';
 
 export default angular.module('app.home',[uirouter])
-.config(routing)
-.controller('TabController',TabController)
-.name;
+    .config(routing)
+    .controller('TabController',TabController)
+    .directive('userInput', userInput)
+    .directive('userToDo', userToDo)
+    .directive('allToDo', allToDo)
+    .name;
